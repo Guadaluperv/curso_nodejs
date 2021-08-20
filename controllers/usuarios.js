7208c85a57f72d48a86310acc5994b045c53c259
Guadalupe Ramirez Vazquez <guadalupemsc.rv@gmail.com>
2021-08-20 02:19:03 -0500

    Separamos rutas y controladores

---
+++ controllers/usuarios.js
@@ -0,0 +1,22 @@
+const {request, response } = require("express");
+
+const usuariosGet = (req = request, res = response) => {
+    res.json ({msg:"Hola a todos desde GET"});
+    };
+
+    const usuariosPost = (req = request, res = response) => {
+        const data = req.body;
+        res.status(201).json({ msg: "Hola a todos desde POST", data });
+    };
+
+    const usuariosPut = (req = request, res = response) => {
+        const data = req.params;
+        res.status(400).json({ msg: "Hola a todos desde PUT", data });
+    };
+
+    const usuariosDelete = (req = request, res = response) => {
+        const data = req.query;
+       res.status(500).json({ msg: "Hola a todos desde DELETE", data });
+    };
+
+    module.exports = { usuariosGet, usuariosPost, usuariosPut, usuariosDelete }; 
