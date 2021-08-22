1698a3231868b39e2137ab37bc0db9b9d321dd9c
Guadalupe Ramirez Vazquez <guadalupemsc.rv@gmail.com>
2021-08-22 18:22:07 -0500

    Agregado BcryptJS para el HASH de la contrseña

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,6 +1,7 @@
 const {request, response } = require("express");
 const pool = require("../db/conexion");
 const usuariosQueries = require("../models/usuarios");
+const bcryptjs = require ("bcryptjs");
 
 const usuariosGet = async (req = request, res = response) => {
     let conn;
@@ -26,13 +27,17 @@ const usuariosGet = async (req = request, res = response) => {
         
         let conn;
 
-       try {
+       
+        try {
+           const salt = bcrytjs.genSaltSync();
+           const passwordHash = bcrytjs.hashSync(password, salt);
            
            conn = await pool.getConnection();
 
-           const usuarios = await conn.query(usuariosQueries.insertUsuario,[nombre,         
+           const usuarios = await conn.query(usuariosQueries.insertUsuario,[
+            nombre,         
              email,
-             password,
+             passwordHash,
              status
             ]);
 
