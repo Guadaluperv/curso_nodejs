3a6179e79fa8ee04e543c25fe0dd2e40635035af
Guadalupe Ramirez Vazquez <guadalupemsc.rv@gmail.com>
2021-08-22 14:14:42 -0500

    Modificaciones a los EnPoints para que coincidan con CRUD

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,22 +1,95 @@
 const {request, response } = require("express");
+const pool = require("../db/conexion");
+const usuariosQueries = require("../models/usuarios");
 
-const usuariosGet = (req = request, res = response) => {
-    res.json ({msg:"Hola a todos desde GET"});
+const usuariosGet = async (req = request, res = response) => {
+    let conn;
+
+    try{
+        conn = await pool.getConnection();
+
+        const usuarios = await conn.query(usuariosQueries.selectUsuarios);
+
+        res.json({usuarios});
+    } catch (error){
+        console.log(error);
+        res
+        .status(500)
+        .json({ msg:"Por favor contacte al administrador.", error });
+    } finally {
+        if (conn) conn.end();
+    }
+        };
+
+    const usuariosPost = async (req = request, res = response) => {
+        const { nombre, email, password, status = 1 } = req.body;
+        
+        let conn;
+
+       try {
+           
+           conn = await pool.getConnection();
+
+           const usuarios = await conn.query(usuariosQueries.insertUsuario,[nombre,         
+             email,
+             password,
+             status
+            ]);
+
+           res.json({ usuarios });
+       } catch (error) 
+       {
+           console.log(error);
+           res
+           .status(500)
+           .json({ msg: "Por favor contacte al administrador.", error });
+       } finally         {
+           if (conn) conn.end();       }
+    };
+        const usuariosPut = async (req = request, res = response) =>   {
+        const { email } = req.query;
+        const {nombre, status } = req.body;
+        
+        let conn;
+
+        try{
+            conn = await pool.getConnection();
+    
+            const usuarios = await conn.query(usuariosQueries.updateUsuario, [
+            nombre,
+            status,
+            email,
+        ]);
+    
+            res.json({usuarios});
+        } catch (error){
+            console.log(error);
+            res
+            .status(500)
+            .json({ msg:"Por favor contacte al administrador.", error });
+        } finally {
+            if (conn) conn.end();
+        }
     };
 
-    const usuariosPost = (req = request, res = response) => {
-        const data = req.body;
-        res.status(201).json({ msg: "Hola a todos desde POST", data });
-    };
+       const usuariosDelete = async (req = request, res = response) => {
+        const { email } = req.query;
+        let conn;
+        
+        try {
+            conn = await pool.getConnection();
 
-    const usuariosPut = (req = request, res = response) => {
-        const data = req.params;
-        res.status(400).json({ msg: "Hola a todos desde PUT", data });
-    };
+            const usuarios = await conn.query(usuariosQueries.deleteUsuario, [email]);
 
-    const usuariosDelete = (req = request, res = response) => {
-        const data = req.query;
-       res.status(500).json({ msg: "Hola a todos desde DELETE", data });
-    };
-
+            res.json({ usuarios });
+        } catch (error) {
+            console.log(error);
+            res
+                .status(500)
+                .json({ msg: "por favor contacte al administrado.", error});
+            } finally {
+              if (conn) conn.end();
+            }
+        };
+       
     module.exports = { usuariosGet, usuariosPost, usuariosPut, usuariosDelete }; 
--- db/conexion.js
+++ db/conexion.js
@@ -2,12 +2,13 @@ const mariadb = require("mariadb");
 
 const config = {
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.DBUSER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     connectionLimit: process.env.CONN_LIMIT,
 };
 
+console.log(config);
 const pool = mariadb.createPool(config);
 
 module.exports = pool;
